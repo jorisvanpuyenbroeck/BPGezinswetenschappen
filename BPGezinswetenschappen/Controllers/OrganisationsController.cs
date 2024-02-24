a0a7e30d4072db06d55c37ad0e78f26246c1f3c8
Joris Van Puyenbroeck <m9407046@student.thomasmore.be>
2024-02-24 23:15:39 +0100

    user management

---
+++ BPGezinswetenschappen/Controllers/OrganisationsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BPGezinswetenschappen.DAL.Data;
+using BPGezinswetenschappen.DAL.Models;
+
+namespace BPGezinswetenschappen.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganisationsController : ControllerBase
+    {
+        private readonly BPContext _context;
+
+        public OrganisationsController(BPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Organisations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Organisation>>> GetOrganisations()
+        {
+            return await _context.Organisations.ToListAsync();
+        }
+
+        // GET: api/Organisations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Organisation>> GetOrganisation(int id)
+        {
+            var organisation = await _context.Organisations.FindAsync(id);
+
+            if (organisation == null)
+            {
+                return NotFound();
+            }
+
+            return organisation;
+        }
+
+        // PUT: api/Organisations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrganisation(int id, Organisation organisation)
+        {
+            if (id != organisation.OrganisationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(organisation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrganisationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Organisations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Organisation>> PostOrganisation(Organisation organisation)
+        {
+            _context.Organisations.Add(organisation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrganisation", new { id = organisation.OrganisationId }, organisation);
+        }
+
+        // DELETE: api/Organisations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrganisation(int id)
+        {
+            var organisation = await _context.Organisations.FindAsync(id);
+            if (organisation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Organisations.Remove(organisation);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrganisationExists(int id)
+        {
+            return _context.Organisations.Any(e => e.OrganisationId == id);
+        }
+    }
+}
